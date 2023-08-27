0c2dc4da192c1379158189871944e0353e3c3934
Luis Ramirez <lerc7689@hotmail.com>
2023-08-27 16:32:51 -0400

    Responsive

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,78 +1,101 @@
 import "./AboutMe.css";
-import aboutMeImg from "../../assets/img/imgAboutMe.jpeg"
+import aboutMeImg from "../../assets/img/imgAboutMe.jpeg";
 // import { useEffect } from "react";
 
-const AboutMe = () =>{
+const AboutMe = () => {
+  window.addEventListener("scroll", function () {
+    var imgContainer = document.querySelector(".imgContainer");
+    var AboutMeInfoContainer = document.querySelector(".AboutMeInfoContainer");
+    var scrollPosition = window.scrollY;
+    var threshold = window.innerHeight * 3; // 100vh
 
-    window.addEventListener('scroll', function() {
-        var imgContainer = document.querySelector(".imgContainer");
-        var AboutMeInfoContainer = document.querySelector(".AboutMeInfoContainer");
-        var scrollPosition = window.scrollY;
-        var threshold = window.innerHeight * 3; // 100vh
+    if ((scrollPosition = threshold)) {
+      imgContainer.classList.add("visible_imgContainer");
+      AboutMeInfoContainer.classList.add("visible_AboutMeInfoContainer");
+    } else {
+      imgContainer.classList.remove("visible_imgContainer");
+      AboutMeInfoContainer.classList.remove("visible_AboutMeInfoContainer");
+    }
+  });
 
-        if (scrollPosition = threshold) {
-            imgContainer.classList.add('visible_imgContainer');
-            AboutMeInfoContainer.classList.add('visible_AboutMeInfoContainer');
-        } else{
-            imgContainer.classList.remove('visible_imgContainer');
-            AboutMeInfoContainer.classList.remove('visible_AboutMeInfoContainer');
-        }
-    });
+  // useEffect(()=>{
 
-    // useEffect(()=>{
-
-    // },[])
-    return(
+  // },[])
+  return (
     <>
-            <a name="aboutMe"></a>
-            <section className="aboutMeContainer" id="aboutMe">
-                <div className="imgContainer">
-                    <img src={aboutMeImg} alt="" />
-                </div>
-                <div className="AboutMeInfoContainer">
-                    <p className="aboutMeMessage">
-                        My name is Luis Eduardo Ramirez C. I am passionate about code, 
-                        I think that not everyone has the happiness of working on what 
-                        they like, so I feel lucky, I like the challenges that programming 
-                        offers and knowing that every day I will learn something new.
-                    </p>
+      <a name="aboutMe"></a>
+      <section className="aboutMeContainer" id="aboutMe">
+        <div className="imgContainer">
+          <img src={aboutMeImg} alt="" />
+        </div>
+        <div className="AboutMeInfoContainer">
+          <p className="aboutMeMessage">
+            My name is Luis Eduardo Ramirez C. I am passionate about code, I
+            think that not everyone has the happiness of working on what they
+            like, so I feel lucky, I like the challenges that programming offers
+            and knowing that every day I will learn something new.
+          </p>
 
-                <ul className="aboutMeList">
-                    <li>
-                    {/* <!-- location --> */}
-                    <span class="about-info-logo"><i class="fas fa-map-marker-alt" aria-hidden="true"></i></span>
-                    <span><b>Location:</b>Santo Domingo, Republica Dominicana</span>
-                    </li>
-                    {/* <!-- interests --> */}
-                    <li>
-                    <span class="about-info-logo"><i class="fas fa-gamepad" aria-hidden="true"></i></span>
-                    <span><b>Interests:</b>Motocross, Excercise, Nature</span>
-                    </li>
-                    {/* <!-- study --> */}
-                    <li>
-                    <span class="about-info-logo"><i class="fas fa-school" aria-hidden="true"></i></span>
-                    <span><b>Study:</b>Academlo</span>
-                    </li>
-                    {/* <!-- degree --> */}
-                    <li>
-                    <span class="about-info-logo"><i class="fas fa-graduation-cap" aria-hidden="true"></i></span>
-                    <span><b>Degree:</b>systems and computer engineer</span>
-                    </li>
-                    {/* <!-- mail --> */}
-                    <li>
-                    <span class="about-info-logo"><i class="fas fa-envelope" aria-hidden="true"></i></span>
-                    <span><b>Mail:</b>lerc7689@hotmail.com </span>
-                    </li>
-                    {/* <!-- phone --> */}
-                    <li>
-                    <span class="about-info-logo"><i class="fas fa-phone-alt" aria-hidden="true"></i></span>
-                    <span><b>Phone:</b>829-721-3784</span>
-                    </li>
-                </ul>
-                </div>
-            </section>
-
-    </>)
-}
+          <ul className="aboutMeList">
+            <li>
+              {/* <!-- location --> */}
+              <span class="about-info-logo">
+                <i class="fas fa-map-marker-alt" aria-hidden="true"></i>
+              </span>
+              <span>
+                <b>Location:</b>Santo Domingo, Republica Dominicana
+              </span>
+            </li>
+            {/* <!-- interests --> */}
+            <li>
+              <span class="about-info-logo">
+                <i class="fas fa-gamepad" aria-hidden="true"></i>
+              </span>
+              <span>
+                <b>Interests:</b>Motocross, Excercise, Nature
+              </span>
+            </li>
+            {/* <!-- study --> */}
+            <li>
+              <span class="about-info-logo">
+                <i class="fas fa-school" aria-hidden="true"></i>
+              </span>
+              <span>
+                <b>Study:</b>Academlo
+              </span>
+            </li>
+            {/* <!-- degree --> */}
+            <li>
+              <span class="about-info-logo">
+                <i class="fas fa-graduation-cap" aria-hidden="true"></i>
+              </span>
+              <span>
+                <b>Degree:</b>systems and computer engineer
+              </span>
+            </li>
+            {/* <!-- mail --> */}
+            <li>
+              <span class="about-info-logo">
+                <i class="fas fa-envelope" aria-hidden="true"></i>
+              </span>
+              <span>
+                <b>Mail:</b>lerc7689@hotmail.com{" "}
+              </span>
+            </li>
+            {/* <!-- phone --> */}
+            <li>
+              <span class="about-info-logo">
+                <i class="fas fa-phone-alt" aria-hidden="true"></i>
+              </span>
+              <span>
+                <b>Phone:</b>829-721-3784
+              </span>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default AboutMe;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,27 +1,35 @@
 import "./Home.css";
-import homeImgContainer from "../../assets/img/homeImg.png"
+import homeImgContainer from "../../assets/img/homeImg.png";
 
-const Home = () =>{
-    return(
+const Home = () => {
+  return (
     <>
-        <a name="home"></a>
-        <div className="homeContainer" id="home">
-            <div className='downloadCV'>
-                <a href="./src/assets/LuisRamirezCV.pdf" download="Luis Ramirez CV">Download CV <br/><br/><i className="fa-solid fa-angles-down"></i></a>
-            </div>
-
-
-            <div className="typeWriter ">
-                <div className='homeImgContainer'>
-                    <img src={homeImgContainer} alt="" />
-                </div>
-                <h1>HEY, <span>I'M LUIS RAMIREZ</span></h1>
-                <p className="homeAnimation">A focused Web Developer building the backend and the Frontend of 
-                                            Websites and Web Applications that leads to the success of the 
-                                            overall product.</p>
-            </div>
+      <a name="home"></a>
+      <div className="homeContainer" id="home">
+        <div className="downloadCV">
+          <a href="./src/assets/LuisRamirezCV.pdf" download="Luis Ramirez CV">
+            Download CV <br />
+            <br />
+            <i className="fa-solid fa-angles-down"></i>
+          </a>
         </div>
-    </>)
-}
+
+        <div className="typeWriter ">
+          <div className="homeImgContainer">
+            <img src={homeImgContainer} alt="" />
+          </div>
+          <h1>
+            HEY, <span>I'M LUIS RAMIREZ</span>
+          </h1>
+          <p className="homeAnimation">
+            A focused Web Developer building the backend and the Frontend of
+            Websites and Web Applications that leads to the success of the
+            overall product.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Home;
